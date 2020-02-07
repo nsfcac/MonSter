aaa073480108812f67001d6d71b3b6dbcf97222b
artlands <lijie0416@gmail.com>
2020-02-07 00:35:59 -0600

    add features

--- slurmapi/slurmapi.py
+++ slurmapi/slurmapi.py
@@ -6,25 +6,11 @@ class Slurm_Job():
         try:
             j = pyslurm.job()
             data = j.get()
+            ids = j.ids()
+            last_update = j.lastUpdate()
             json_data["data"] = data
-        except Exception as e:
-            error = {
-                "title": "Python Exception",
-                "meta": {"args": e.args}
-            }
-            json_data["errors"] = [error]
-            print("Error: " + self.__str__() + " : " + e.message)
-        
-        return json_data
-
-
-class Slurm_JobStep():
-    def get(self):
-        json_data = {}
-        try:
-            js = pyslurm.jobstep()
-            data = js.get()
-            json_data["data"] = data
+            json_data["ids"] = ids
+            json_data["updateTime"] = last_update
         except Exception as e:
             error = {
                 "title": "Python Exception",
@@ -42,7 +28,11 @@ class Slurm_Node():
         try:
             n = pyslurm.node()
             data = n.get()
+            ids = n.ids()
+            last_update = n.lastUpdate()
             json_data["data"] = data
+            json_data["ids"] = ids
+            json_data["updateTime"] = last_update
         except Exception as e:
             error = {
                 "title": "Python Exception",
