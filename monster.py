4df1c47084c868e960eb6698d85d493e1c72a73f
artlands <lijie0416@gmail.com>
2020-02-24 14:06:46 -0600

    monster fetching test

--- monster.py
+++ monster.py
@@ -1,4 +1,7 @@
 import json
+
+from influxdb import InfluxDBClient
+
 from conf_parser import parse_conf, check_metrics
 from slurmapi.fetch_slurm import fetch_slurm
 
@@ -6,7 +9,21 @@ from slurmapi.fetch_slurm import fetch_slurm
 def main():
     config = parse_conf()
     try:
+        # Initialize influxdb
+        host = config["influxdb"]["host"]
+        port = config["influxdb"]["port"]
+        user = config["influxdb"]["user"]
+        password = config["influxdb"]["password"]
+        dbname = config["influxdb"]["dbname"]
+        client = InfluxDBClient(host, port, user, password, dbname)
+
+        # Fetch slurm information
         slurm_info = fetch_slurm(config["metrics"])
+
+        # Write points into influxdb
+        client.write_points(slurm_info["job_info"])
+        client.write_points(slurm_info["node_info"])
+        client.write_points(slurm_info["stat_info"])
     except Exception as err:
         print(err)
     return 
