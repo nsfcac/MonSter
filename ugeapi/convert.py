1be9892f13184a732f4e9a1555733adcb2e9819f
artlands <lijie0416@gmail.com>
2020-03-03 12:09:23 -0600

    test ugeapi

--- ugeapi/convert.py
+++ ugeapi/convert.py
@@ -4,5 +4,4 @@ def get_hostip(hostname: str) -> str:
     """
     if '-' in hostname:
         h0, h1, h2 = hostname.split('-')
-        return '10.101.' + h1 + '.' + h2
-    return None
+        return '10.101.' + h1 + '.' + h2.split('.')[0]
