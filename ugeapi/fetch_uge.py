80f926e877ae509454adbcada93b71a38761ac9a
artlands <lijie0416@gmail.com>
2020-03-03 13:50:44 -0600

    test ugeapi

--- ugeapi/fetch_uge.py
+++ ugeapi/fetch_uge.py
@@ -22,9 +22,11 @@ def fetch_uge(config: object, session: object, ugeapi_adapter: object) -> object
     Fetch metrics from UGE api
     """
     # Get executing hosts and jobs running on the cluster
-    exechost = get_exechosts(config, session, ugeapi_adapter)
-    host = get_host_detail(config, session, ugeapi_adapter, exechost[0])
-    print(host)
+    exechosts = get_exechosts(config, session, ugeapi_adapter)
+    print(exechosts)
+    print(exechosts[0])
+    # host = get_host_detail(config, session, ugeapi_adapter, exechost[0])
+    # print(host)
     # jobs = get_jobs(config, session, ugeapi_adapter)
     # job = get_job_detail(config, session, ugeapi_adapter, jobs[0])
     # print(job)
