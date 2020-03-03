fdcbf200ec04e1986cbe6dd293d8f64b83ad3dcf
artlands <lijie0416@gmail.com>
2020-03-03 15:24:02 -0600

    test ugeapi

--- ugeapi/fetch_uge.py
+++ ugeapi/fetch_uge.py
@@ -27,10 +27,10 @@ def fetch_uge(config: object) -> object:
         # Get executing hosts and jobs running on the cluster
         exechosts = get_exechosts(uge_url, session, ugeapi_adapter)
         # jobs = get_jobs(config, session, ugeapi_adapter)
-        args = zip(repeat(uge_url), repeat(session), repeat(ugeapi_adapter), exechosts)
-        print(list(args)[0])
-        # with multiprocessing.Pool(processes=cpu_count) as pool:
-        #     results = pool.starmap(get_host_detail, zip(repeat(config, session, ugeapi_adapter)), exechosts)
+        pool_args = zip(repeat(uge_url), repeat(session), repeat(ugeapi_adapter), exechosts)
+        with multiprocessing.Pool(processes=cpu_count) as pool:
+            results = pool.starmap(get_host_detail, pool_args)
+        print(results[0])
         # host = get_host_detail(config, session, ugeapi_adapter, exechosts[0])
         # print(host)
         # job = get_job_detail(config, session, ugeapi_adapter, jobs[0])
