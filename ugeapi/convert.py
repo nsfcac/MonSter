07a41f3f489f1e5eb14cbf41914cd64247f0d9ee
artlands <lijie0416@gmail.com>
2020-04-03 16:04:59 -0500

    test ugeapi

--- ugeapi/convert.py
+++ ugeapi/convert.py
@@ -4,3 +4,10 @@ def get_hostip(hostname: str) -> str:
     """
     h0, h1, h2 = hostname.split('-')
     return '10.101.' + h1 + '.' + h2.split('.')[0]
+
+def purify_joblist(jobList: list) -> list:
+    """
+    Extract job id from jobList
+    """
+    job_list = [job["id"] for job in jobList]
+    return job_list
