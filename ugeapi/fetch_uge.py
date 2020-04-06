84850d5ec2688c66240f283b6943f26daf904375
artlands <lijie0416@gmail.com>
2020-04-06 09:52:51 -0500

    test get host point

--- ugeapi/fetch_uge.py
+++ ugeapi/fetch_uge.py
@@ -69,24 +69,24 @@ def fetch_uge(config: object) -> object:
 #----------------------------- End Host Points ---------------------------------
 
 #-------------------------------- Job Points -----------------------------------
-            # Get jobs detail in parallel
-            pool_job_args = zip(repeat(uge_url), repeat(session), repeat(ugeapi_adapter), jobs)
-            with multiprocessing.Pool(processes=cpu_count) as pool:
-                job_data = pool.starmap(get_job_detail, pool_job_args)
+            # # Get jobs detail in parallel
+            # pool_job_args = zip(repeat(uge_url), repeat(session), repeat(ugeapi_adapter), jobs)
+            # with multiprocessing.Pool(processes=cpu_count) as pool:
+            #     job_data = pool.starmap(get_job_detail, pool_job_args)
             
-            for index, job in enumerate(jobs):
-                jobs_info[job] = job_data[index]
+            # for index, job in enumerate(jobs):
+            #     jobs_info[job] = job_data[index]
 
-            # Process job info (job_id:str, jobs_info: object, time: int)
-            process_job_args = (jobs, repeat(jobs_info), repeat(epoch_time))
-            with multiprocessing.Pool(processes=cpu_count) as pool:
-                processed_job_info = pool.starmap(process_job, process_job_args)
+            # # Process job info (job_id:str, jobs_info: object, time: int)
+            # process_job_args = (jobs, repeat(jobs_info), repeat(epoch_time))
+            # with multiprocessing.Pool(processes=cpu_count) as pool:
+            #     processed_job_info = pool.starmap(process_job, process_job_args)
 
-            for index, job in enumerate(jobs):
-                job_point[job] = processed_job_info[index]
+            # for index, job in enumerate(jobs):
+            #     job_point[job] = processed_job_info[index]
 
-            # total_elapsed = float("{0:.4f}".format(time.time() - query_start))
-            print(json.dumps(job_point, indent=4))
+            # # total_elapsed = float("{0:.4f}".format(time.time() - query_start))
+            # print(json.dumps(job_point, indent=4))
 #---------------------------- End Job Points -----------------------------------
     except Exception as err:
         print(err)
