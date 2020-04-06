efd5bb4ad70ca5a9b50319bf42ed73b8c905e4a2
artlands <lijie0416@gmail.com>
2020-04-06 15:18:14 -0500

    test aggregated_node_jobs

--- ugeapi/process_uge.py
+++ ugeapi/process_uge.py
@@ -12,55 +12,56 @@ def process_host(host_id:str, host_info: object, time: int) -> list:
         host_data = host_info[host_id]
         host_ip = get_hostip(host_id)
 
-        # CPUUsage
-        cpuusage = float("{0:.2f}".format(host_data["resourceNumericValues"]["np_load_avg"]))
-        cpuusage_point = {
-            "measurement": "UGE",
-            "tags": {
-                "Label": "CPUUsage",
-                "NodeId": host_ip,
-            },
-            "time": time,
-            "fields": {
-                "Reading": cpuusage
+        if host_data:
+            # CPUUsage
+            cpuusage = float("{0:.2f}".format(host_data["resourceNumericValues"]["np_load_avg"]))
+            cpuusage_point = {
+                "measurement": "UGE",
+                "tags": {
+                    "Label": "CPUUsage",
+                    "NodeId": host_ip,
+                },
+                "time": time,
+                "fields": {
+                    "Reading": cpuusage
+                }
             }
-        }
 
-        # MemUsage
-        mem_free = host_data["resourceNumericValues"]["mem_free"]
-        mem_total = host_data["resourceNumericValues"]["mem_total"]
-        memusage = float("{0:.2f}".format( (mem_total-mem_free)/mem_total ))
-        memusage_point = {
-            "measurement": "UGE",
-            "tags": {
-                "Label": "MemUsage",
-                "NodeId": host_ip,
-            },
-            "time": time,
-            "fields": {
-                "Reading": memusage
+            # MemUsage
+            mem_free = host_data["resourceNumericValues"]["mem_free"]
+            mem_total = host_data["resourceNumericValues"]["mem_total"]
+            memusage = float("{0:.2f}".format( (mem_total-mem_free)/mem_total ))
+            memusage_point = {
+                "measurement": "UGE",
+                "tags": {
+                    "Label": "MemUsage",
+                    "NodeId": host_ip,
+                },
+                "time": time,
+                "fields": {
+                    "Reading": memusage
+                }
             }
-        }
-        # NodeJobs
-        joblist = [str(job["id"]) for job in host_data["jobList"]]
-        jobset = list(set(joblist))
-        joblist_point = {
-            "measurement": "NodeJobs",
-            "tags": {
-                "NodeId": host_ip,
-            },
-            "time": time,
-            "fields": {
-                "JobList": str(jobset)
+            # NodeJobs
+            joblist = [str(job["id"]) for job in host_data["jobList"]]
+            jobset = list(set(joblist))
+            joblist_point = {
+                "measurement": "NodeJobs",
+                "tags": {
+                    "NodeId": host_ip,
+                },
+                "time": time,
+                "fields": {
+                    "JobList": str(jobset)
+                }
             }
-        }
 
-        points = [cpuusage_point, memusage_point, joblist_point]
+            points = [cpuusage_point, memusage_point, joblist_point]
 
-        all_data = {
-            "dpoints": points,
-            "joblist": joblist
-        }
+            all_data = {
+                "dpoints": points,
+                "joblist": joblist
+            }
     except Exception as err:
         print(err)
     
