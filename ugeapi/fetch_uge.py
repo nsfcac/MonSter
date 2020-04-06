b2b1258c7b6f69e6b0d9ea6b78efbcca923c4493
artlands <lijie0416@gmail.com>
2020-04-06 11:46:09 -0500

    test get_hostip

--- ugeapi/fetch_uge.py
+++ ugeapi/fetch_uge.py
@@ -80,7 +80,10 @@ def fetch_uge(config: object) -> object:
             with multiprocessing.Pool(processes=cpu_count) as pool:
                 processed_node_jobs = pool.starmap(process_node_jobs, process_node_jobs_args)
             # print(processed_node_jobs)
-            print(json.dumps(processed_node_jobs, indent=4))
+            # print(json.dumps(processed_node_jobs, indent=4))
+
+            for host in exechosts:
+                print(get_hostip(host))
 
             # # Get jobs detail in parallel
             # pool_job_args = zip(repeat(uge_url), repeat(session), repeat(ugeapi_adapter), jobs)
