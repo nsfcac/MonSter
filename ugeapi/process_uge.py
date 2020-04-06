adac0e88616a054b5d97e0ad558e7c557d76159e
artlands <lijie0416@gmail.com>
2020-04-06 13:18:06 -0500

    test aggregated_node_jobs

--- ugeapi/process_uge.py
+++ ugeapi/process_uge.py
@@ -137,6 +137,7 @@ def aggregate_node_jobs(processed_node_jobs: list) -> dict:
                 if job not in jobset:
                     jobset.append(job)
                     job_data[job] = item[job]
+                    print("flag")
                 else:
                     pre_totalnodes = job_data[job]["totalnodes"]
                     pre_nodelist = job_data[job]["nodelist"]
