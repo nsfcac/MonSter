16a7c4d307f4c28d61e844d0a23c1ba5cd68b7fb
artlands <lijie0416@gmail.com>
2020-04-08 11:16:05 -0500

    fetch_bmc

--- redfishapi/fetch_bmc.py
+++ redfishapi/fetch_bmc.py
@@ -32,9 +32,8 @@ def get_hostip(hostlist_config: str) -> list:
     hostlist = []
     try:
         with open(hostlist_config, "r") as hostlist_file:
-            hostname_str = hostlist_file.read()
-            hostname = json.loads(hostname_str)
-            hostlist = [host.split(":")[0][1:] for host in hostname]
+            hostname_list = hostlist_file.read()[1:-1].split(", ")
+            hostlist = [host.split(":")[0][1:] for host in hostname_list]
     except Exception as err:
         print(err)
 
