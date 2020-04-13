4ae88ab07a2ece930334dff59f62f7c1cfd6d1c5
artlands <lijie0416@gmail.com>
2020-04-13 18:41:06 -0500

    test uge

--- ugeapi/test_uge.py
+++ ugeapi/test_uge.py
@@ -27,5 +27,7 @@ ugeapi_adapter = HTTPAdapter(config["max_retries"])
 with requests.Session() as session:
     epoch_time = int(round(time.time() * 1000000000))
     host_detail = get_host_detail(config, uge_url, session, ugeapi_adapter)
-    processed_host_detail = process_host(host_detail[0], epoch_time)
-    print(json.dumps(processed_host_detail, indent=4))
+    exechosts = [item["hostname"] for item in host_detail]
+    print(json.dumps(exechosts, indent=4))
+    # processed_host_detail = process_host(host_detail[0], epoch_time)
+    # print(json.dumps(processed_host_detail, indent=4))
