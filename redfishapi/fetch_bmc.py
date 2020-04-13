73fb73f09f067f29d49498bbaae5bc0dc359bf15
artlands <lijie0416@gmail.com>
2020-04-13 12:01:24 -0500

    test fetch_bmc

--- redfishapi/fetch_bmc.py
+++ redfishapi/fetch_bmc.py
@@ -44,7 +44,8 @@ def fetch_bmc(config: object, hostlist: list) -> object:
         timeout = aiohttp.ClientTimeout(total=config["timeout"]["total"], connect=config["timeout"]["connect"])
 
         urls = generate_urls(hostlist)
-        asyncio.get_event_loop().run_until_complete(download_all_bmc(urls, conn, auth, timeout, bmc_info))
+        print(urls)
+        # asyncio.get_event_loop().run_until_complete(download_all_bmc(urls, conn, auth, timeout, bmc_info))
 
         # print(json.dumps(bmc_info, indent = 4))
 
