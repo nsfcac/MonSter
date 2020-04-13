eecedd4a597f1b7e690c5153c874d82dca074416
artlands <lijie0416@gmail.com>
2020-04-13 13:48:10 -0500

    test monster

--- monster.py
+++ monster.py
@@ -55,17 +55,16 @@ def write_db(client: object, config: object, hostlist: list) -> None:
 
         uge_job_points = fetch_uge(config["uge"])["all_job_points"]
         
-        # for job_point in uge_host_points:
-        #     try:
-        #         job_id = job_point["tags"]["JobId"]
-        #         if not check_job(client, job_id):
-        #             all_points.append(job_point)
-        #     except:
-        #         pass
+        for job_point in uge_host_points:
+            job_id = job_point["tags"]["JobId"]
+            print(job_id)
+            if not check_job(client, job_id):
+                all_points.append(job_point)
+            
 
         # Write points into influxdb
         # client.write_points(all_points)
-        print(json.dumps(uge_job_points, indent=4))
+        # print(json.dumps(uge_job_points, indent=4))
         print("Done!")
     except Exception as err:
         print(err)
