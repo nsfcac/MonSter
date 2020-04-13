9e0276fbbbd439bac7a004177081cfaa9d72544b
artlands <lijie0416@gmail.com>
2020-04-13 18:45:19 -0500

    test uge

--- ugeapi/test_uge.py
+++ ugeapi/test_uge.py
@@ -1,6 +1,10 @@
 import json
 import time
+
 import requests
+import multiprocessing
+from itertools import repeat
+from requests.exceptions import Timeout
 from requests.adapters import HTTPAdapter
 
 from fetch_uge import get_host_detail
@@ -23,11 +27,26 @@ config = {
 uge_url = "http://" + config["host"] + ":" + config["port"]
 ugeapi_adapter = HTTPAdapter(config["max_retries"])
 
+all_host_points = []
+node_jobs = {}
+
+cpu_count = multiprocessing.cpu_count()
 
 with requests.Session() as session:
     epoch_time = int(round(time.time() * 1000000000))
     host_detail = get_host_detail(config, uge_url, session, ugeapi_adapter)
+    
+    # Process host info
+    process_host_args = zip(host_detail, repeat(epoch_time))
+    with multiprocessing.Pool(processes=cpu_count) as pool:
+        processed_host_detail = pool.starmap(process_host, process_host_args)
+
     exechosts = [item["hostname"] for item in host_detail]
-    print(json.dumps(exechosts, indent=4))
+    for index, host in enumerate(exechosts):
+        try:
+            all_host_points.extend(processed_host_detail[index]["dpoints"])
+            node_jobs[host] = processed_host_detail[index]["joblist"]
+        except Exception as err:
+            print(err)
     # processed_host_detail = process_host(host_detail[0], epoch_time)
-    # print(json.dumps(processed_host_detail, indent=4))
+    print(json.dumps(all_host_points, indent=4))
