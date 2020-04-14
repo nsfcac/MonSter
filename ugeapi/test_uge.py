7faafeb73d24edaab902e98cdb8a53f511f7f853
artlands <lijie0416@gmail.com>
2020-04-13 21:15:09 -0500

    test

--- ugeapi/test_uge.py
+++ ugeapi/test_uge.py
@@ -36,24 +36,23 @@ with requests.Session() as session:
     epoch_time = int(round(time.time() * 1000000000))
     host_detail = get_host_detail(config, uge_url, session, ugeapi_adapter)
     
-    current_jobs = get_current_jobs(config, uge_url, session, ugeapi_adapter)
-    # # Process host info
-    # process_host_args = zip(host_detail, repeat(epoch_time))
-    # with multiprocessing.Pool(processes=cpu_count) as pool:
-    #     processed_host_detail = pool.starmap(process_host, process_host_args)
+    # current_jobs = get_current_jobs(config, uge_url, session, ugeapi_adapter)
+    # Process host info
+    process_host_args = zip(host_detail, repeat(epoch_time))
+    with multiprocessing.Pool(processes=cpu_count) as pool:
+        processed_host_detail = pool.starmap(process_host, process_host_args)
 
-    # exechosts = [item["hostname"] for item in host_detail]
-    # for index, host in enumerate(exechosts):
-    #     try:
-    #         all_host_points.extend(processed_host_detail[index]["dpoints"])
-    #         node_jobs[host] = processed_host_detail[index]["joblist"]
-    #     except Exception as err:
-    #         print(err)
-    # # processed_host_detail = process_host(host_detail[0], epoch_time)
+    exechosts = [item["hostname"] for item in host_detail]
+    for index, host in enumerate(exechosts):
+        try:
+            all_host_points.extend(processed_host_detail[index]["dpoints"])
+            node_jobs[host] = processed_host_detail[index]["joblist"]
+        except Exception as err:
+            print(err)
+    processed_host_detail = process_host(host_detail[0], epoch_time)
     # for k, v in node_jobs.items():
     #     for id in v:
     #         if "." in id:
     #             print("ARRAY Job")
     # print("End")
-    print(json.dumps(current_jobs, indent=4))
-    print(json.dumps(host_detail, indent=4))
+    print(json.dumps(node_jobs, indent=4))
