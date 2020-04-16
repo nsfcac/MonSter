219090d3660cc79839a44922e83d1826828cdd72
artlands <lijie0416@gmail.com>
2020-04-16 17:41:19 -0500

    test ugeapi

--- ugeapi/process_uge.py
+++ ugeapi/process_uge.py
@@ -153,18 +153,11 @@ def aggregate_node_jobs(node_jobs: dict) -> dict:
             for job, job_detail in jobs.items():
                 if job not in jobset:
                     jobset.append(job)
-                    nodelist = [job_detail["fields"]["nodelist"] + "-" + str(job_detail["fields"]["cpucores"])]
                     jobs_data[job] = job_detail
-                    jobs_data[job]["fields"].update({
-                        "nodelist": nodelist
-                    })
                 else:
                     cpucores = jobs_data[job]["fields"]["cpucores"] + job_detail["fields"]["cpucores"]
                     totalnodes = jobs_data[job]["fields"]["totalnodes"] + 1
-                    this_node_list = job_detail["fields"]["nodelist"] + "-" + str(job_detail["fields"]["cpucores"])
-                    if not jobs_data[job]["fields"]["nodelist"]:
-                        print(job)
-                    nodelist = jobs_data[job]["fields"]["nodelist"].append(this_node_list)
+                    nodelist = jobs_data[job]["fields"]["nodelist"].extend(job_detail["fields"]["nodelist"])
                     jobs_data[job]["fields"].update({
                         "cpucores": cpucores,
                         "totalnodes": totalnodes,
