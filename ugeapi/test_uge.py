2eefebfc15e00990205d1382a317445d6858a0aa
artlands <lijie0416@gmail.com>
2020-04-16 17:59:34 -0500

    test ugeapi

--- ugeapi/test_uge.py
+++ ugeapi/test_uge.py
@@ -61,5 +61,5 @@ with requests.Session() as session:
     #         print("Empty Job List")
     # print("End")
     print(json.dumps(processed_host_detail, indent=4))
-    # print(json.dumps(node_jobs, indent=4))
+    print(json.dumps(node_jobs, indent=4))
     # print(json.dumps(all_job_points, indent=4))
