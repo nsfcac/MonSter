3fb5fceb28d83546b622216b2684e545bc1ce8a2
artlands <lijie0416@gmail.com>
2020-04-16 17:21:14 -0500

    test ugeapi

--- ugeapi/process_uge.py
+++ ugeapi/process_uge.py
@@ -157,7 +157,8 @@ def aggregate_node_jobs(node_jobs: dict) -> dict:
                 else:
                     cpucores = jobs_data[job]["fields"]["cpucores"] + job_detail["fields"]["cpucores"]
                     totalnodes = jobs_data[job]["fields"]["totalnodes"] + 1
-                    nodelist = jobs_data[job]["fields"]["nodelist"].extend([job_detail["fields"]["nodelist"] + "-" + str(job_detail["fields"]["cpucores"])])
+                    this_node_list = job_detail["fields"]["nodelist"] + "-" + str(job_detail["fields"]["cpucores"])
+                    nodelist = jobs_data[job]["fields"]["nodelist"].append(this_node_list)
                     jobs_data[job]["fields"].update({
                         "cpucores": cpucores,
                         "totalnodes": totalnodes,
