08cb713fdb6123e7d2a6e8fc19e6f0eb44745071
artlands <lijie0416@gmail.com>
2020-04-17 18:16:06 -0500

    test 1- hosts

--- redfishapi/test_bmc.py
+++ redfishapi/test_bmc.py
@@ -95,7 +95,7 @@ def get_hostlist(hostlist_dir: str) -> list:
     return hostlist
 
 
-hostlist = get_hostlist(config["hostlist"])[:10]
+hostlist = get_hostlist(config["hostlist"])[:100]
 # hostlist = ["10.101.1.1"]
 
 fetch_bmc(config, hostlist)
