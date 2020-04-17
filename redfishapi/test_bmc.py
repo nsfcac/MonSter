bcebf5a6c403816069dd522ab2dfb1602fbae5d3
artlands <lijie0416@gmail.com>
2020-04-17 17:20:11 -0500

    test redfishapi

---
+++ redfishapi/test_bmc.py
@@ -0,0 +1,99 @@
+import json
+import time
+import requests
+# import multiprocessing
+import asyncio
+import aiohttp
+
+from itertools import repeat
+from requests.exceptions import Timeout
+from requests.adapters import HTTPAdapter 
+
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+# from redfishapi.process_bmc import process_bmc
+
+# For test single function
+from process_bmc import process_bmc
+
+config = {
+    "user": "password",
+    "password": "monster",
+    "timeout": {
+        "connect": 2,
+        "total": 10
+    },
+    "max_retries": 3,
+    "ssl_verify": False,
+    "hostlist": "../hostlist"
+}
+
+
+def fetch_bmc(config: object, hostlist: list) -> object:
+    """
+    Fetch bmc metrics from Redfish, average query and process time is: 11.57s
+    """
+    bmc_metrics = []
+    try:
+        conn = aiohttp.TCPConnector(limit=config["max_retries"], ssl=config["ssl_verify"])
+        auth = aiohttp.BasicAuth(config["user"], config["password"])
+        timeout = aiohttp.ClientTimeout(total=config["timeout"]["total"], connect=config["timeout"]["connect"])
+
+        urls = generate_urls(hostlist)
+
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(download_all_bmc(urls, conn, auth, timeout, bmc_metrics))
+
+        print(json.dumps(bmc_metrics, indent=4))
+
+    except Exception as err:
+        print("fetch_bmc ERROR: ", end = " ")
+        print(err)
+    
+    return 
+
+
+async def download_bmc(session: object, url: str) -> None:
+    async with session.get(url) as response:
+        return await response.json()
+
+
+async def download_all_bmc(urls: list, conn: object, auth: object, timeout: object, bmc_metrics: list) -> None:
+    async with aiohttp.ClientSession(connector= conn, auth=auth, timeout=timeout) as session:
+        bmc_metrics = []
+        for url in urls:
+            metric = await download_bmc(session, url)
+            bmc_metrics.append(metric)
+
+
+def generate_urls(hostlist:list) -> list:
+    urls = []
+    # Thermal URLS
+    for host in hostlist:
+        thermal_url = "https://" + host + "/redfish/v1/Chassis/System.Embedded.1/Thermal/"
+        urls.append(thermal_url)
+    for host in hostlist:
+        power_url = "https://" + host + "/redfish/v1/Chassis/System.Embedded.1/Power/"
+        urls.append(power_url)
+    return urls
+
+
+def get_hostlist(hostlist_dir: str) -> list:
+    """
+    Parse host IP from file
+    """
+    hostlist = []
+    try:
+        with open(hostlist_dir, "r") as hostlist_file:
+            hostname_list = hostlist_file.read()[1:-1].split(", ")
+            hostlist = [host.split(":")[0][1:] for host in hostname_list]
+    except Exception as err:
+        print(err)
+    return hostlist
+
+
+# hostlist = get_hostlist(config["hostlist"])
+hostlist = ["10.101.1.1", "10.101.9.17"]
+
+fetch_bmc(config, hostlist)
