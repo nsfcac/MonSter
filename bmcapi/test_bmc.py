6992e5dbb084439816bb5ebe2dc6d2e5cc676a63
artlands <lijie0416@gmail.com>
2020-04-19 16:54:50 -0500

    test bmcapi

--- bmcapi/test_bmc.py
+++ bmcapi/test_bmc.py
@@ -37,7 +37,7 @@ logging.basicConfig(
 config = {
     "user": "password",
     "password": "monster",
-    "timeout": 6,
+    "timeout": 10,
     "max_retries": 1,
     "ssl_verify": False,
     "hostlist": "../hostlist"
