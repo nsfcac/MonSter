c5b9495406c30f8275ed43031e94dcaea2ccca29
artlands <lijie0416@gmail.com>
2020-04-23 14:15:06 -0500

    test_bmc_requests

--- bmcapi/test_bmc_requests.py
+++ bmcapi/test_bmc_requests.py
@@ -119,7 +119,7 @@ def get_bmc_detail(config: dict, bmc_url: str, session: object, bmcapi_adapter: 
     return
 
 
-# hostlist = get_hostlist(config["hostlist"])
-hostlist = ["10.101.1.1"]
+hostlist = get_hostlist(config["hostlist"])
+# hostlist = ["10.101.1.1"]
 
 fetch_bmc(config, hostlist)
