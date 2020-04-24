f59762a297e2a38e7d0c7b9124ed4b13f19343ac
artlands <lijie0416@gmail.com>
2020-04-23 21:44:36 -0500

    test monster

--- monster.py
+++ monster.py
@@ -5,7 +5,7 @@ from influxdb import InfluxDBClient
 
 from helper import parse_config, check_config, get_hostlist
 from ugeapi.fetch_uge import fetch_uge
-# from redfishapi.fetch_bmc import fetch_bmc
+from bmcapi.fetch_bmc import fetch_bmc
 
 
 def main():
@@ -20,8 +20,6 @@ def main():
         # Initialize influxdb
         host = config["influxdb"]["host"]
         port = config["influxdb"]["port"]
-        user = config["influxdb"]["user"]
-        password = config["influxdb"]["password"]
         dbname = config["influxdb"]["database"]
         hostlist = get_hostlist(config["hostlistdir"])
         client = InfluxDBClient(host=host, port=port, database=dbname)
@@ -31,13 +29,17 @@ def main():
 
         write_db(client, config, hostlist)
 
-        # schedule.every(freq).seconds.do(write_db, client, config)
+        schedule.every(freq).seconds.do(write_db, client, config)
 
         # while 1:
         #     schedule.run_pending()
         #     time.sleep(freq)
 
-        # print("DONE!")
+        for i in range(10):
+            schedule.run_pending()
+            time.sleep(freq)
+
+        print("DONE!")
     except Exception as err:
         print(err)
     return 
@@ -45,9 +47,9 @@ def main():
 def write_db(client: object, config: object, hostlist: list) -> None:
     all_points = []
     try:
-        # # Fetch BMC information
-        # bmc_points = fetch_bmc(config["redfish"], hostlist)
-        # all_points.extend(bmc_points)
+        # Fetch BMC information
+        bmc_points = fetch_bmc(config["redfish"], hostlist)
+        all_points.extend(bmc_points)
 
         # Fetch UGE information
         uge_host_points = fetch_uge(config["uge"])["all_host_points"]
@@ -60,7 +62,6 @@ def write_db(client: object, config: object, hostlist: list) -> None:
             if not check_job(client, job_id):
                 all_points.append(job_point)
             
-
         # Write points into influxdb
         client.write_points(all_points)
         # print(json.dumps(uge_job_points, indent=4))
