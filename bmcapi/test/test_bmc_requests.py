fd0c3aa7d8a4c54c078db718a209fd7f40dfc88a
artlands <lijie0416@gmail.com>
2020-04-25 22:45:30 -0500

    test requests

--- bmcapi/test/test_bmc_requests.py
+++ bmcapi/test/test_bmc_requests.py
@@ -30,8 +30,8 @@ config = {
     "user": "root",
     "password": "nivipnut",
     "timeout": {
-        "connect": 30,
-        "read": 60
+        "connect": 15,
+        "read": 40
     },
     "max_retries": 3,
     "ssl_verify": False,
