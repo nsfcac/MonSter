02dedbb3073f9d8645f4512f4b8236e4a002df6f
artlands <lijie0416@gmail.com>
2020-04-28 10:04:46 -0500

    update monster

--- monster.py
+++ monster.py
@@ -1,8 +1,10 @@
+#!/usr/bin python3.6
+
 import json
 import time
 from influxdb import InfluxDBClient
 import logging
-
+import schedule
 from helper import parse_config, check_config, get_hostlist
 from ugeapi.fetch_uge import fetch_uge
 from bmcapi.fetch_bmc import fetch_bmc
@@ -14,6 +16,8 @@ logging.basicConfig(
     datefmt='%Y-%m-%d %H:%M:%S %Z'
 )
 
+prev_joblist = []
+
 
 def main():
     config = parse_config()
@@ -24,6 +28,8 @@ def main():
         return
     
     try:
+        global prev_joblist
+
         # Initialize influxdb
         host = config["influxdb"]["host"]
         port = config["influxdb"]["port"]
@@ -32,68 +38,65 @@ def main():
         client = InfluxDBClient(host=host, port=port, database=dbname)
 
         # Monitoring frequency
-        # freq = config["frequency"]
+        freq = config["frequency"]
         
-        write_db(client, config, hostlist)
+        schedule.every(freq).minutes.do(write_db, client, config, hostlist)
+
+        # while True:
+        #     schedule.run_pending()
+        #     time.sleep(1)
+
+        for i in range(10):
+            schedule.run_pending()
+            time.sleep(1)
 
     except Exception as err:
         print(err)
     return 
 
 def write_db(client: object, config: object, hostlist: list) -> None:
+    global prev_joblist
     all_points = []
     curr_joblist = []
-
     # print(json.dumps(prev_joblist, indent=4))
+
     try:
-        with open("previous_jobs.json", "r+") as prev_job_file:
-            # load previous job list
-            try:
-                prev_joblist = json.load(prev_job_file)
-            except:
-                prev_joblist = []
+        # Fetch UGE information
+        uge_info = fetch_uge(config["uge"])
 
-            try:
-                # Fetch UGE information
-                uge_info = fetch_uge(config["uge"])
+        uge_host_points = uge_info["all_host_points"]
+        all_points.extend(uge_host_points)
 
-                uge_host_points = uge_info["all_host_points"]
-                all_points.extend(uge_host_points)
+        uge_job_points = uge_info["all_job_points"]
+        uge_epoch_time = uge_info["epoch_time"]
+        
+        for job_point in uge_job_points:
+            job_id = job_point["tags"]["JobId"]
+            curr_joblist.append(job_id)
+            if not fetch_job(client, job_id):
+                all_points.append(job_point)
 
-                uge_job_points = uge_info["all_job_points"]
-                uge_epoch_time = uge_info["epoch_time"]
-                
-                for job_point in uge_job_points:
-                    job_id = job_point["tags"]["JobId"]
-                    curr_joblist.append(job_id)
-                    if not fetch_job(client, job_id):
-                        all_points.append(job_point)
+        # Compare current job list with previous job list and update finish time
+        for job_id in prev_joblist:
+            # This job is finished 
+            if job_id not in curr_joblist:
+                updated_job = update_job(client, job_id, uge_epoch_time)
+                if updated_job:
+                    all_points.append(updated_job)
 
-                # Compare current job list with previous job list and update finish time
-                for job_id in prev_joblist:
-                    # This job is finished 
-                    if job_id not in curr_joblist:
-                        updated_job = update_job(client, job_id, uge_epoch_time)
-                        if updated_job:
-                            all_points.append(updated_job)
+        # Update previous jobs
+        prev_joblist = curr_joblist
 
-                # Fetch BMC information
-                bmc_points = fetch_bmc(config["redfish"], hostlist)
-                all_points.extend(bmc_points)
+        # Fetch BMC information
+        bmc_points = fetch_bmc(config["redfish"], hostlist)
+        all_points.extend(bmc_points)
 
-                # Write points into influxdb
-                client.write_points(all_points)
-
-                # Update previous jobs
-                prev_job_file.seek(0)
-                json.dump(curr_joblist, prev_job_file)
-                # print(json.dumps(all_points, indent=4))
-            except:
-                logging.error("Cannot write data points to influxDB")
+        # Write points into influxdb
+        client.write_points(all_points)
+        # print(json.dumps(all_points, indent=4))
 
     except:
-        logging.error("Do not have the previous jobs record")
-    return
+        logging.error("Cannot write data points to influxDB")
 
 
 def fetch_job(client: object, job_id: str) -> object:
