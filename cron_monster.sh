9986e9800a0f406aa2a55870996c32334a3eaf82
artlands <lijie0416@gmail.com>
2020-04-27 23:36:38 -0500

    test cron monster

--- cron_monster.sh
+++ cron_monster.sh
@@ -10,10 +10,10 @@ if [ $1 == 0 ]
 then
     echo > previous_jobs.json
     echo "Start MonSTer cron job..."
-    crontab cornfile
+    crontab /home/lijie/MonSter/cornfile
     crontab -l
 else
     echo "Stop MonSTer cron job..."
-    crontab -l > cornfile
+    crontab -l > /home/lijie/MonSter/cornfile
     crontab -r
 fi
