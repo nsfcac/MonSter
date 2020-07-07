370304a3ba269c326c0e928bd91d359b0b3e68c1
artlands <lijie0416@gmail.com>
2020-07-07 11:37:11 -0500

    add AsyncioRequests

--- classes/AsyncioRequests.py
+++ classes/AsyncioRequests.py
@@ -10,17 +10,25 @@ class AsyncioRequests:
     from aiohttp import ClientSession
 
 
-    def __init__(self, timeout: tuple = (15, 45)):
+    def __init__(self, timeout: tuple = (15, 45), max_retries: int = 3):
+        self.retry = 0
         self.loop = self.asyncio.get_event_loop()
         self.timeout = self.aiohttp.ClientTimeout(connect=timeout[0], total=timeout[1])
+        self.max_retries = max_retries
     
     async def __fetch_json(self, url: str, session: ClientSession) -> dict:
         """
         Get request wrapper to fetch json data from API
         """
-        resp = await session.request(method='GET', url=url)
-        resp.raise_for_status()
-        return await resp.json()
+        try:
+            resp = await session.request(method='GET', url=url)
+            resp.raise_for_status()
+            return await resp.json()
+        except (TimeoutError):
+            self.retry += 1
+            if self.retry >= self.max_retries:
+                raise TimeoutError()
+            return await self.__fetch_json(url, session)
 
 
     async def __requests(self, urls: list) -> list:
