196b3cd3e79bec6c16af867ba3bb9fa5f6291778
artlands <lijie0416@gmail.com>
2020-07-07 19:55:47 -0500

    update ProcessGlances

--- glancesapi/fetch_glances.py
+++ glancesapi/fetch_glances.py
@@ -8,6 +8,12 @@ from monster.helper import parse_config, parse_hostlist
 
 
 def fetch_glances() -> object:
+    """
+    fetch OS metrics from glances API. 
+    Examples of using glances API:
+    curl http://10.10.1.4:61208/api/3/pluginslist | python -m json.tool
+    curl http://10.10.1.4:61208/api/3/percpu | python -m json.tool
+    """
     config = parse_config('../config.yml')
     try:
         api = config["glances"]["api"]
@@ -16,9 +22,11 @@ def fetch_glances() -> object:
 
         urls = ["http://" + host + ":" + str(port) + api for host in hosts]
 
+        # Asynchronously fetch glances metrics from all nodes
         glances = AsyncioRequests()
         metrics = glances.bulk_fetch(urls)
 
+        # Process metrics and generate data points
         process = ProcessGlances(metrics[0], hosts[0])
         datapoints = process.get_datapoints()
         print(json.dumps(datapoints, indent = 4))
