066ace029e692ae4f92959eb8bc69a3d0106e393
artlands <lijie0416@gmail.com>
2020-07-10 16:35:06 -0500

    modify ugeapi

--- ugeapi/get_hostip.py
+++
@@ -1,8 +0,0 @@
-def get_hostip(hostname: str) -> str:
-    """
-    Convert hostname('compute-1-1.localdomain') to ip address (10.101.1.1)
-    """
-    if "-" in hostname:
-        h0, h1, h2 = hostname.split('-')
-        return '10.101.' + h1 + '.' + h2.split('.')[0]
-    return None
