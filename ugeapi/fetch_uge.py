87a6366bd8d487deae5bb3115de434651d1f5e8b
artlands <lijie0416@gmail.com>
2020-07-11 13:05:54 -0500

    test ProcessUge

--- ugeapi/fetch_uge.py
+++ ugeapi/fetch_uge.py
@@ -111,7 +111,8 @@ def aggregate(all_data: dict) -> list:
                 })
     all_jobpoints = list(all_job_info.values())
 
-    print(f"Job List length: {len(all_jobpoints)}")
+    print(f"Job Points length: {len(all_jobpoints)}")
+    print(f"Job List length: {len(list(set(all_job_list)))}")
     # all_datapoints.extend(all_jobpoints)
 
     return all_datapoints
