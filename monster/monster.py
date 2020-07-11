63a34003be7be50b88faffae68e16b6b75784d78
artlands <lijie0416@gmail.com>
2020-07-11 18:43:54 -0500

    test monster

--- monster/monster.py
+++ monster/monster.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 
 import json
+import time
+import threading
+import schedule
+from influxdb import InfluxDBClient
 import sys
 sys.path.append('../')
 
@@ -22,16 +26,52 @@ def main():
     if not check_config(config):
         return
     try:
-        all_datapoints = []
+        # Monitoring frequency
+        freq = config["frequency"]
 
         bmc_config = config['bmc']
         uge_config = config['uge']
+        influx_config = config['influxdb']
+
+        # Initialize influxdb
+        host = influx_config["host"]
+        port = influx_config["port"]
+        dbname = influx_config["database"]
+
+        influx_client = InfluxDBClient(host=host, port=port, database=dbname)
+
+        # Schedule run_monster
+        schedule.every(freq).seconds.do(run_monster, monster, 
+                                        bmc_config, uge_config, influx_client)
         
-        all_datapoints = fetch_datapoints(bmc_config, uge_config)
+        while True:
+            try:
+                schedule.run_pending()
+                time.sleep(schedule.idle_seconds())
+            except KeyboardInterrupt:
+                break   
 
         return
     except Exception as err:
         print(err)
+    return
+
+
+def run_monster(monster, bmc_config: dict, uge_config: dict, influx_client: object):
+    """
+    Create monster threads
+    """
+    job_thread = threading.Thread(target=monster, 
+                                  args=(bmc_config, uge_config, influx_client))
+    job_thread.start()
+
+
+def monster(bmc_config: dict, uge_config: dict, influx_client: object) -> None:
+    """
+    Fetch and write datapoints into influxdb
+    """
+    all_datapoints = fetch_datapoints(bmc_config, uge_config)
+    influx_client.write_points(all_datapoints)
     return
 
 
@@ -68,10 +108,10 @@ def fetch_datapoints(bmc_config: dict, uge_config: dict) -> list:
     # Contatenate all data points
     all_datapoints = bmc_datapoints + uge_datapoints + job_datapoints
 
-    print(f"BMC data points length: {len(bmc_datapoints)}")
-    print(f"UGE data points length: {len(uge_datapoints)}")
-    print(f"Job data points length: {len(job_datapoints)}")
-    print(json.dumps(all_datapoints, indent=4))
+    # print(f"BMC data points length: {len(bmc_datapoints)}")
+    # print(f"UGE data points length: {len(uge_datapoints)}")
+    # print(f"Job data points length: {len(job_datapoints)}")
+    # print(json.dumps(all_datapoints, indent=4))
 
     return all_datapoints
 
