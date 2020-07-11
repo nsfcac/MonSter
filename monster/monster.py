07d01aea83616a8294791ad41a9c6bc501ad5348
artlands <lijie0416@gmail.com>
2020-07-11 18:01:47 -0500

    test ProcessUge

--- monster/monster.py
+++ monster/monster.py
@@ -9,6 +9,12 @@ from ugeapi.fetch_uge import fetch_uge
 from sharings.utils import parse_config, check_config
 
 
+# Temporarily store previous job list; compared with the current job list and
+# estimate the Finish time of the job; If it is in the previous list but not in
+# the current job list, its finish time should before the current time stamp
+prev_joblist = []
+
+
 def main():
     # Read configuration file
     config = parse_config('./config.yml')
@@ -20,22 +26,32 @@ def main():
 
         bmc_config = config['bmc']
         uge_config = config['uge']
-
-        # Fetch BMC and uge data points
-        bmc_datapoints = fetch_bmc(bmc_config)
-        uge_datapoints = fetch_uge(uge_config)
-
-        # Aggregate data points
-        all_datapoints = bmc_datapoints + uge_datapoints["datapoints"]
-        print(f"All datapoints length: {len(all_datapoints)}")
-
-        all_jobspoints = uge_datapoints["jobspoints"]
-        print(f"All jobspoints length: {len(all_jobspoints)}")
         
         return
     except Exception as err:
         print(err)
     return
+
+
+def fetch_datapoints(bmc_config: dict, uge_config: dict) -> list:
+    # Fetch BMC and uge data points
+    bmc_datapoints = fetch_bmc(bmc_config)
+    uge_datapoints = fetch_uge(uge_config)
+
+    # Aggregate data points
+    all_datapoints = bmc_datapoints + uge_datapoints["datapoints"]
+
+    all_jobspoints = uge_datapoints["jobspoints"]
+    curr_joblist = get_joblist(all_jobspoints)
+
+    # 
+
+def get_joblist(jobspoints: list) -> list:
+    """
+    Get job list from the jobs points
+    """
+    joblist = [ job["tags"]["JobId"] for job in jobspoints ]
+    return joblist
 
 
 if __name__ == '__main__':
