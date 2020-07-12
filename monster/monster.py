489405cea457f390f40e29505bde26a3926faa5e
artlands <lijie0416@gmail.com>
2020-07-12 00:00:37 -0500

    test monster

--- monster/monster.py
+++ monster/monster.py
@@ -34,60 +34,52 @@ def main():
         return
     try:
         # Monitoring frequency
-        freq = config["frequency"]
+        # freq = config["frequency"]
 
         bmc_config = config['bmc']
         uge_config = config['uge']
         influx_config = config['influxdb']
 
-        fetch_datapoints(bmc_config, uge_config)
-
         # Initialize influxdb
         host = influx_config["host"]
         port = influx_config["port"]
         dbname = influx_config["database"]
-
         influx_client = InfluxDBClient(host=host, port=port, database=dbname)
 
-        # Schedule run_monster
-        schedule.every(freq).seconds.do(run_monster, monster, 
-                                        bmc_config, uge_config, influx_client)
-        
-        while True:
-            try:
-                schedule.run_pending()
-                time.sleep(schedule.idle_seconds())
-            except KeyboardInterrupt:
-                break   
+        # Fetch data points
+        all_datapoints = fetch_datapoints(bmc_config, uge_config)
+
+        # Write data points into influxdb
+        influx_client.write_points(all_datapoints)
 
         return
     except Exception as err:
-        logging.error(f"main error : {err}")
+        logging.error(f"monster main error : {err}")
     return
 
 
-def run_monster(monster, bmc_config: dict, uge_config: dict, influx_client: object):
-    """
-    Create monster threads
-    """
-    try:
-        job_thread = threading.Thread(target=monster, 
-                                    args=(bmc_config, uge_config, influx_client))
-        job_thread.start()
-    except Exception as err:
-        logging.error(f"run_monster error : {err}")
+# def run_monster(monster, bmc_config: dict, uge_config: dict, influx_client: object):
+#     """
+#     Create monster threads
+#     """
+#     try:
+#         job_thread = threading.Thread(target=monster, 
+#                                     args=(bmc_config, uge_config, influx_client))
+#         job_thread.start()
+#     except Exception as err:
+#         logging.error(f"run_monster error : {err}")
 
 
-def monster(bmc_config: dict, uge_config: dict, influx_client: object) -> None:
-    """
-    Fetch and write datapoints into influxdb
-    """
-    try:
-        all_datapoints = fetch_datapoints(bmc_config, uge_config)
-        influx_client.write_points(all_datapoints)
-    except Exception as err:
-        logging.error(f"monster error : {err}")
-    return
+# def monster(bmc_config: dict, uge_config: dict, influx_client: object) -> None:
+#     """
+#     Fetch and write datapoints into influxdb
+#     """
+#     try:
+#         all_datapoints = fetch_datapoints(bmc_config, uge_config)
+#         influx_client.write_points(all_datapoints)
+#     except Exception as err:
+#         logging.error(f"monster error : {err}")
+#     return
 
 
 def fetch_datapoints(bmc_config: dict, uge_config: dict) -> list:
