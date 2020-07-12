3602313c1e26db9ef4c27589d338169b46e0a801
artlands <lijie0416@gmail.com>
2020-07-12 01:07:06 -0500

    test cron monster

--- controller.sh
+++ controller.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 readonly sourceFile="$PWD/MonSter/env/bin/activate"
+echo ${sourceFile}
 source ${sourceFile}
-
+python -V
 python $PWD/MonSter/monster.py
+
+# */1 * * * * /home/monster/MonSter/controller.sh
