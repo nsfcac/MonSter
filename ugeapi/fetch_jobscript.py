bad62d2920046433d850fd5ff462e6a253e08eec
artlands <lijie0416@gmail.com>
2020-08-12 20:58:26 -0500

    test fetch_jobscript

--- ugeapi/fetch_jobscript.py
+++ ugeapi/fetch_jobscript.py
@@ -52,21 +52,21 @@ def fetch_jobscript(uge_config: dict) -> list:
             for job in all_jobinfo:
                 job_id = job['id']
                 job_info = job['info']
-                saved_path = '/home/monster/MonSter/ugeapi/data/' + job_id
+                saved_path = '/home/monster/MonSter/ugeapi/data/jobscripts/' + job_id
 
                 try:
                     if '.' in job_id:
                         job_id = job_id.split('.')[0]
                         saved_path = '/home/monster/MonSter/ugeapi/data/jobscripts/' + job_id
                     script_path = get_script_path_exec(uge_config, job_id, job_info)
-                    print(f"script_path : {script_path}")
                     if script_path:
+                        print(f"job_id : {job_id}, script_path : {script_path}")
                         ftp_client.get(script_path, saved_path)
                 except:
                     try:
                         script_path = get_script_path_work(uge_config, job_info)
-                        print(f"script_path : {script_path}")
                         if script_path:
+                            print(f"job_id : {job_id}, script_path : {script_path}")
                             ftp_client.get(script_path, saved_path)
                     except:
                         pass
