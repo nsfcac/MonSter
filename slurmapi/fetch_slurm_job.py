773cd17512c879020a3447816e351c89c31552b7
artlands <lijie0416@gmail.com>
2020-10-29 22:32:22 -0500

    test fetch slurm job string

--- slurmapi/fetch_slurm_job.py
+++ slurmapi/fetch_slurm_job.py
@@ -26,9 +26,12 @@ def main():
     # Split strings by line, and discard the first line that indicates the metrics name
     rtn_str_arr = rtn_str.splitlines()[1:]
 
-    # Generate all job data dict
+    # Get all job data dict
     job_dict_all = process_job_dict(format, rtn_str_arr)
-    print(json.dumps(job_dict_all, indent=4))
+
+    # Aggregate job data
+    aggregated_job_dict = aggregate_job_dict(job_dict_all)
+    print(json.dumps(aggregated_job_dict, indent=4))
     # for i in rtn_str_arr:
     #     processed = str_2_json(format, i)
     #     print(json.dumps(processed, indent=4))
@@ -47,19 +50,6 @@ def str_2_json(format: list, job_str: str, queue: object) -> dict:
         job_data.update({
             format[i]: job_str_arr[i]
         })
-    
-    # # Unfold metrics in treusageintot and tresusageoutot
-    # folded_metrics = job_data.get("tresusageintot", None)
-    # if folded_metrics:
-    #     unfolded_metrics = unfold(folded_metrics)
-    #     job_data.update(unfolded_metrics)
-    #     job_data.pop("tresusageintot")
-    
-    # folded_metrics = job_data.get("tresusageouttot", None)
-    # if folded_metrics:
-    #     unfolded_metrics = unfold(folded_metrics)
-    #     job_data.update(unfolded_metrics)
-    #     job_data.pop("tresusageouttot")
     
     job_dict = {
         job_data["jobid"]: job_data
@@ -101,7 +91,58 @@ def process_job_dict(format: list, rtn_str_arr: list) -> dict:
 
     for p in procs:
         p.join()
+
     return job_dict_all
+
+
+def aggregate_job_dict(job_dict_all: dict) -> dict:
+    """
+    Aggregate jobid.batch with jobid, and unfold several metrics under the same 
+    attribute, such as "tresusageintot", "tresusageouttot".
+    """
+    aggregated_job_dict = {}
+    job_id_list = job_dict_all.keys()
+    for job_id in job_id_list:
+        if ".batch" not in job_id:
+            job_id_batch = job_id + ".batch"
+            job_data = merge_metrics(job_dict_all[job_id], job_dict_all[job_id_batch])
+            
+            # Unfold metrics in treusageintot and tresusageoutot
+            folded_metrics = job_data.get("tresusageintot", None)
+            if folded_metrics:
+                unfolded_metrics = unfold(folded_metrics)
+                job_data.update(unfolded_metrics)
+                job_data.pop("tresusageintot")
+            
+            folded_metrics = job_data.get("tresusageouttot", None)
+            if folded_metrics:
+                unfolded_metrics = unfold(folded_metrics)
+                job_data.update(unfolded_metrics)
+                job_data.pop("tresusageouttot")
+            
+            aggregated_job_dict.update({
+                job_id: job_data
+            })
+    return aggregated_job_dict
+    
+
+def merge_metrics(job_metircs: dict, batch_metrics: dict) -> dict:
+    """
+    Merge metrics under jobid.batch to metrics under jobid.
+    """
+    merged_metrics = {}
+    for key, value in job_metircs.items():
+        if value == None:
+            merged_metrics.update({
+                key: batch_metrics[key]
+            })
+        else:
+            merged_metrics.update({
+                key: value
+            })
+    return merged_metrics
+
+
 
 if __name__ == '__main__':
     main()
