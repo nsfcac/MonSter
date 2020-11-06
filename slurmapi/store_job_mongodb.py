dee2d5e40de2c019b7bbe37b062d941a5d6364b1
artlands <lijie0416@gmail.com>
2020-11-05 21:17:06 -0600

    test store job

--- slurmapi/store_job_mongodb.py
+++ slurmapi/store_job_mongodb.py
@@ -1,19 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+This module calls the fetch_slurm_job function to fetch the job data from slurm
+and saved the data records into mongodb.
+
+Jie Li (jie.li@ttu.edu)
+"""
+import sys
 import pymongo
 
-from fetch_slurm_job import fetch_slurm_job
+sys.path.append('../')
 
-myclient = pymongo.MongoClient("mongodb://localhost:27017")
+from slurmapi.fetch_slurm_job import fetch_slurm_job
+from sharings.utils import parse_config
 
-mydb = myclient["slurm_job_data"]
+def store_job_mongodb():
+    # Read configuration file
+    config_path = './config.yml'
+    config = parse_config(config_path)
 
-# dblist = myclient.list_database_names()
-# if "slurm_job_data" in dblist:
-#     print("The database exists.")
+    # Setting mongodb parameters
+    client = config["mongodb"]["client"]
+    database = config["mongodb"]["database"]
+    collection = config["mongodb"]["collection"]
 
-collection = mydb["job_reports"]
+    # Initialize mongodb
+    mongodb_client = pymongo.MongoClient(client)
+    mongodb_db = mongodb_client[database]
+    mongodb_col = mongodb_db[collection]
 
-job_data = fetch_slurm_job()
+    # Check if the target database is in mongodb
+    dblist = mongodb_client.list_database_names()
+    if database not in dblist:
+        print("The database does not exists. MonSTer will create one for you!")
 
-x = collection.insert_many(job_data)
+    job_data = fetch_slurm_job()
 
-print(x.inserted_ids)
+    print(job_data)
+    # # Insert data records
+    # if job_data:
+    #     x = collection.insert_many(job_data)
+    #     print(x.inserted_ids)
+    # else:
+    #     print("Fail to get job data!")
+    # return
+
+if __name__ == '__main__':
+    store_job_mongodb()
