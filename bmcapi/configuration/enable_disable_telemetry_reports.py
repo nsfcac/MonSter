c67e33bb7ea25d43fc1e9ec93a4d3cb0c2a70ea4
artlands <lijie0416@gmail.com>
2020-12-02 20:15:39 -0600

    debug bmcapi configuration

--- bmcapi/configuration/enable_disable_telemetry_reports.py
+++ bmcapi/configuration/enable_disable_telemetry_reports.py
@@ -35,13 +35,25 @@ def main():
     config = parse_config('../../config.yml')
     nodes = parse_nodelist(config['bmc']['nodelist'])
     
-    # Ask username and password
-    user = input ("iDRAC username: ")
+    # Ask setting, username and password
+    setting = input("Enabled/Disable telemetry reports: ")
+    setting = setting.lower()
+    if setting not in ['enable', 'disable', 'e', 'd']:
+        print("Invalid setting. Please select Enable or Disable")
+        return
+
+    user = input("iDRAC username: ")
     password = getpass(prompt='iDRAC password: ')
     
     # Get attributes
     attributes = get_attributes(config, nodes[0], user, password)
+    # print(json.dumps(attributes, indent=4))
 
+    # Enable or disable telemetry arrtibutes
+    result = enable_disable_telemetry_reports(config, nodes[0], user, 
+                                       password, attributes, setting)
+    
+    attributes = get_attributes(config, nodes[0], user, password)
     print(json.dumps(attributes, indent=4))
 
 
@@ -69,8 +81,42 @@ def get_attributes(config: dict, ip: str, user: str, password: str) -> dict:
     return attributes
 
 
-def enable_disable_telemetry_reports():
-    return
+def enable_disable_telemetry_reports(config: dict, ip: str, 
+                                     user: str, password: str, 
+                                     attributes: dict, setting: str) -> bool:
+    """
+    Enable or disable telemetry reports
+    """
+    uri = f'https://{ip}/redfish/v1/Managers/iDRAC.Embedded.1/Attributes'
+    headers = {'content-type': 'application/json'}
+
+    if setting in ['enable', 'e']:
+        setting_value = 'Enable'
+    else:
+        setting_value = 'Disable'
+
+    updated_attributes = {k: setting_value for k in attributes.keys()}
+    patch_data = json.dumps({"Attributes": updated_attributes})
+
+    adapter = HTTPAdapter(max_retries=config['bmc']['max_retries'])
+    with requests.Session() as session:
+        session.mount(uri, adapter)
+        try:
+            response = session.patch(
+                uri,
+                auth = (user, password),
+                verify = config['bmc']['ssl_verify'], 
+                headers = headers,
+                data = patch_data
+            )
+            if response.status_code != 200:
+                logging.error(f"Fail to update telemetry attributes on {ip}: \
+                               {response.status_code}")
+                return False
+        except Exception as err:
+            logging.error(f"Fail to update telemetry attributes on {ip}: {err}")
+
+    return True
 
 
 if __name__ == '__main__':
