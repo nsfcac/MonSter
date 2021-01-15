ea301208f12e33686486e5079368e260f62af34f
Jie Li (jie.li@ttu.edu) <li29729@hugo.hpcc.ttu.edu>
2021-01-14 23:15:25 -0600

    update set_telemetry_reports

--- bmcapi/TelemetryScripts/set_telemetry_reports.py
+++ bmcapi/TelemetryScripts/set_telemetry_reports.py
@@ -3,6 +3,31 @@
 """
     This module uses Redfish API to enable or disable all telemetry reports of
     iDRACs where the IP addresses are configured in the config.yml.
+    The available telemetry reports are:
+     - PSUMetrics
+     - PowerStatistics
+     - PowerMetrics
+     - CUPS
+     - CPUMemMetrics
+     - AggregationMetrics
+     - ThermalSensor
+     - ThermalMetrics
+     - StorageSensor
+     - StorageDiskSMARTData
+     - Sensor
+     - FCSensor
+     - NICStatistics
+     - NICSensor
+     - MemorySensor
+     - SerialLog
+     - CPUSensor
+     - CPURegisters
+     - FanSensor
+     - GPUStatistics
+     - NVMeSMARTData
+     - GPUMetrics
+     - FPGASensor
+    Comment out the report name in config.yml to disable
 
 Jie Li (jie.li@ttu.edu)
 """
@@ -36,44 +61,36 @@ logging.basicConfig(
 def main():
     # Read configuratin file
     config = parse_config('../../config.yml')
-    nodes = parse_nodelist(config['bmc']['nodelist'])
+    nodes = parse_nodelist(config['bmc']['iDRAC9_nodelist'])
+    enabled_reports = config['bmc']['telemetry_report']
     
-    setting, user, password = get_user_input()
+    # print(nodes)
+    # print(enabled_reports)
+    user, password = get_user_input()
 
     # Get attributes
-    attributes = get_attributes(config, nodes[0], user, password)
+    attributes = get_attributes(config, nodes[1], user, password)
     # print(json.dumps(attributes, indent=4))
 
-    # Enable or disable telemetry reports asynchronously
+    # Update telemetry report settings asynchronously
     loop = asyncio.get_event_loop()
     status = loop.run_until_complete(set_telemetry_reports(config, nodes, 
                                                   user, password, 
-                                                  attributes, setting))
+                                                  attributes, enabled_reports))
     loop.close()
 
     # Report status
-    status_report(status, nodes, setting)
+    status_report(status, nodes, enabled_reports, attributes)
 
 
 def get_user_input() -> tuple:
     """
     Ask setting, username and password
     """
-    setting = input("--> Enable/Disable telemetry reports: ")
-    setting = setting.lower()
-    if setting not in ['enable', 'disable', 'e', 'd']:
-        print("--> Invalid setting. Please select Enable or Disable")
-        return
-    else:
-        if setting in ['enable', 'e']:
-            setting = 'Enabled'
-        else:
-            setting = 'Disabled'
-
     user = input("--> iDRAC username: ")
     password = getpass(prompt='--> iDRAC password: ')
 
-    return(setting, user, password)
+    return(user, password)
 
 
 def get_attributes(config: dict, ip: str, user: str, password: str) -> dict:
@@ -102,7 +119,7 @@ def get_attributes(config: dict, ip: str, user: str, password: str) -> dict:
 
 async def set_telemetry_reports(config: dict, nodes: list, 
                                 user: str, password: str,
-                                attributes: dict, setting: str) -> None:
+                                attributes: dict, enabled_reports: list) -> None:
     """
     Enable or disable telemetry reports asynchronously
     """
@@ -114,18 +131,20 @@ async def set_telemetry_reports(config: dict, nodes: list,
                              auth=auth, timeout=timeout) as session:
         tasks = []
         for ip in nodes:
-            tasks.append(enable_disable_reports(ip, attributes, setting, session))
+            tasks.append(enable_disable_reports(ip, attributes, enabled_reports, session))
         return await asyncio.gather(*tasks)
 
 
-async def enable_disable_reports(ip: str, attributes: dict, setting: str, 
+async def enable_disable_reports(ip: str, attributes: dict, enabled_reports: list, 
                                  session: ClientSession) -> int:
     """
     Enable or disable telemetry reports
     """
     url = f'https://{ip}/redfish/v1/Managers/iDRAC.Embedded.1/Attributes'
     headers = {'content-type': 'application/json'}
-    updated_attributes = {k: setting for k in attributes.keys()}
+
+    updated_attributes = update_attributes(attributes, enabled_reports)
+
     patch_data = {"Attributes": updated_attributes}
     try:
         resp = await session.request(method='PATCH', url=url, 
@@ -140,21 +159,57 @@ async def enable_disable_reports(ip: str, attributes: dict, setting: str,
         logging.error(f"Fail to update telemetry attributes on {ip}: {err}")
 
 
-def status_report(status: list, nodes: list, setting: str) -> None:
+def update_attributes(attributes: dict, enabled_reports: list) -> dict:
+    """
+    Update attributes
+    """
+    updated_attributes = {}
+    copy_reports_setting = list(enabled_reports)
+    try:
+        for k in attributes.keys():
+            setting = 'Disabled'
+
+            for report in copy_reports_setting:
+                if report in k:
+                    setting='Enabled'
+                    copy_reports_setting.remove(report)
+                    break
+
+            updated_attributes.update({
+                k: setting
+            })
+
+        updated_attributes.update({
+            "Telemetry.1.EnableTelemetry": "Enabled"
+        })
+        return updated_attributes
+
+    except Exception as err:
+        logging.error(f"Fail to update attributes : {err}")
+
+
+def status_report(status: list, nodes: list, enabled_reports: list, attributes: dict) -> None:
     """
     Generate status report for the setting
     """
     total_cnt = len(nodes)
     success_cnt = status.count(200)
     fail_nodes = []
+    updated_attributes = update_attributes(attributes, enabled_reports)
 
-    print(f"--> {success_cnt} out of {total_cnt} have been {setting.upper()} the telemetry reports successfully!")
+    print(f"--> {success_cnt} out of {total_cnt} have been set the telemetry reports successfully!")
+
+    print("The updated attributes are: ")
+    print(json.dumps(updated_attributes, indent=4))
 
     selection = input("--> Press l to display the failed nodes; press other key to quit: ")
     
     if selection in ['L', 'l']:
-        fail_nodes = [nodes[i] for i in status if status[i]!=200 ]
-        print(f"--> {fail_nodes}")
+        try:
+            fail_nodes = [nodes[i] for i,j in enumerate(status) if j!=200 ]
+            print(f"--> {fail_nodes}")
+        except Exception as err:
+            logging.error(f"Fail to generate status report: {err}")
     else:
         return
 
