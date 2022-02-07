54a465d6f94763a7e4023588f8f0b1efadfa0fb7
Cristiano Caon <cristiano.e.caon@gmail.com>
2022-02-07 17:13:10 -0600

    feat: aggregation script for every 3 days

---
+++ aggregation/create_table.py
@@ -0,0 +1,21 @@
+import logging
+
+
+def create_table(conn: object, table: str) -> any:
+    try:
+        cursor = conn.cursor()
+
+        create_table_query = f"""CREATE TABLE IF NOT EXISTS aggr_{table} (
+                time TIMESTAMPTZ NOT NULL,
+                nodeid INT4 NOT NULL,
+                source TEXT,
+                fqdd TEXT,
+                value FLOAT4,
+                FOREIGN KEY (nodeid) REFERENCES public.nodes(nodeid));"""
+
+        cursor.execute(create_table_query)
+        conn.commit()
+        cursor.close()
+
+    except Exception as err:
+        logging.error(f"Create tables error : {err}")
---
+++ aggregation/init.py
@@ -0,0 +1,39 @@
+import time
+import logging
+import psycopg2
+
+from dotenv import dotenv_values
+from create_table import create_table
+from aggregate_insert_query import aggregate_insert_query
+
+tsdb_config = dotenv_values(".env")
+CONNECTION = f"dbname={tsdb_config['DBNAME']} user={tsdb_config['USER']} password={tsdb_config['PASSWORD']} options='-c search_path=idrac8'"
+
+logging.basicConfig(
+    level=logging.ERROR,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S %Z'
+)
+
+
+def main():
+
+    start_time = time.time()
+
+    tables = [
+        "rpmreading",
+        "systempowerconsumption",
+        "temperaturereading",
+    ]
+
+    conn = psycopg2.connect(CONNECTION)
+
+    for table in tables:
+        create_table(conn, table)
+        aggregate_insert_query(conn, table, 5)
+
+    print("\n--- %s seconds ---" % (time.time() - start_time))
+
+
+if __name__ == "__main__":
+    main()
