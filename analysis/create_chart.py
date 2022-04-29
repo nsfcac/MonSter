c87b9432daeb2c68f1a3b62d0253a94d89b4d68a
Cristiano Caon <cristiano.e.caon@gmail.com>
2022-04-29 18:11:09 -0500

    improved create_chart analysis

--- analysis/create_chart.py
+++ analysis/create_chart.py
@@ -1,26 +1,27 @@
 import matplotlib.pyplot as plt
 
 
-def create_chart(records: list, deduplicated_records: list) -> None:
+def create_chart(records1: list, records2: list) -> None:
 
     records_x = []
     records_y = []
-    for record in records:
-        if record[3] == "CPU1 Temp" and record[1] == 21 and record[4] > 0:
+    for record in records1:
+        if record[3] == "FAN_4" and record[1] == 20:
             records_x.append(record[0])
             records_y.append(record[4])
 
-    deduplicated_records_x = []
-    deduplicated_records_y = []
-    for deduplicated_record in deduplicated_records:
-        if deduplicated_record[3] == "CPU1 Temp" and deduplicated_record[1] == 21 and deduplicated_record[4] > 0:
-            deduplicated_records_x.append(deduplicated_record[0])
-            deduplicated_records_y.append(deduplicated_record[4])
+    records2_x = []
+    records2_y = []
+    for record in records2:
+        if record[3] == "FAN_4" and record[1] == 20:
+            records2_x.append(record[0])
+            records2_y.append(record[4])
 
     fig = plt.figure()
-    plt.plot_date(records_x, records_y, 'g-', label='Aggregated')
-    plt.plot_date(deduplicated_records_x,
-                  deduplicated_records_y, 'b-', label='Aggregated & Deduplicated', linestyle='dashed')
+    plt.style.use('seaborn')
+    plt.plot_date(records_x, records_y, '-', label='Aggregated')
+    plt.plot_date(records2_x, records2_y, '-', label='Reconstructed')
     fig.autofmt_xdate()
-    plt.legend()
-    plt.savefig('regular_vs_deduplicated_temperaturereading.png')
+    plt.legend(loc=2)
+    plt.savefig(
+        './analysis/results/aggregated_vs_reconstructed_fan4_20.png')
