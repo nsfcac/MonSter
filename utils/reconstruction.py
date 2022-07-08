dd0201dc140a5750576fb0dc3ee68ccc3006ccc3
Cristiano Caon <cristiano.e.caon@gmail.com>
2022-07-07 20:53:38 -0500

    modified reconstruction algorithm

--- utils/reconstruction.py
+++ utils/reconstruction.py
@@ -1,50 +1,55 @@
 import logging
 
-from datetime import timedelta
+from datetime import datetime, timedelta
 
 
-def reconstruction(records: list) -> list:
+def reconstruction(records: list, end_date: datetime, time_gap: int = 10) -> list:
 
-    previous_reading = {}
-    reconstructed_records = []
+    node_label_records = {}
+    reconstructed = []
 
     try:
         for record in records:
-            timestamp, nodeid, source, label, avg_value, min_value, max_value = record
+            nodeid = record[1]
+            label = record[3]
+            if nodeid not in node_label_records:
+                node_label_records[nodeid] = {}
 
-            if ((avg_value is None or avg_value < 0) or (min_value is None or min_value < 0)
-                    or (max_value is None or max_value < 0)):
-                reconstructed_records.append(record)
+            if label not in node_label_records[nodeid]:
+                node_label_records[nodeid][label] = [record]
                 continue
 
-            if nodeid not in previous_reading:
-                previous_reading[nodeid] = {}
+            node_label_records[nodeid][label].append(record)
 
-            if label not in previous_reading[nodeid]:
-                previous_reading[nodeid][label] = timestamp
-                reconstructed_records.append(record)
-            else:
-                previous_timestamp = previous_reading[nodeid][label]
-                reconstructed_timestamp = previous_timestamp + \
-                    timedelta(minutes=10)
+        for label_records in node_label_records.values():
+            for records in label_records.values():
+                reconstructed.append(records[0])
+                for i in range(1, len(records)):
+                    curr_record = records[i]
+                    curr_time = curr_record[0]
+                    prev_record = records[i - 1]
+                    prev_time = prev_record[0]
 
-                while reconstructed_timestamp < timestamp:
-                    reconstructed_record = [
-                        reconstructed_timestamp,
-                        nodeid,
-                        source,
-                        label,
-                        avg_value,
-                        min_value,
-                        max_value
-                    ]
-                    reconstructed_records.append(reconstructed_record)
-                    reconstructed_timestamp += timedelta(minutes=10)
+                    recon_time = prev_time + \
+                        timedelta(minutes=time_gap)
 
-                reconstructed_records.append(record)
-                previous_reading[nodeid][label] = timestamp
+                    while recon_time < curr_time:
+                        recon_record = (prev_time, *prev_record[1:])
+                        reconstructed.append(recon_record)
+                        recon_time += timedelta(minutes=time_gap)
+
+                    reconstructed.append(curr_record)
+
+                    if i == (len(records) - 1):
+                        recon_time = curr_time + \
+                            timedelta(minutes=time_gap)
+                        while recon_time < end_date:
+                            recon_record = (
+                                curr_time, *curr_record[1:])
+                            reconstructed.append(recon_record)
+                            recon_time += timedelta(minutes=time_gap)
 
     except Exception as err:
         logging.error(f"Reconstruction error : {err}")
 
-    return reconstructed_records
+    return reconstructed
