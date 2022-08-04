7b64de743a0c7c8f484e53d70300b2b4b7f66faa
Cristiano Caon <cristiano.e.caon@gmail.com>
2022-08-03 20:38:08 -0500

    improved function

--- utils/calculate_tolerance.py
+++ utils/calculate_tolerance.py
@@ -4,7 +4,11 @@ import statistics
 
 
 def calculate_tolerance(records: list) -> dict:
+    """Calculates tolerances from given records list using Coefficient of Variation.
 
+    :param list records: original metrics.
+    :return dict: tolerances for each metric type
+    """
     tolerances = {}
 
     try:
@@ -12,16 +16,13 @@ def calculate_tolerance(records: list) -> dict:
             node_id = record[1]
             label = record[3]
             value = record[4]
-
             if node_id not in tolerances:
                 tolerances[node_id] = {}
-
             if label not in tolerances[node_id]:
                 tolerances[node_id][label] = []
-
             if value is not None and value > 0:
                 tolerances[node_id][label].append(value)
-
+                
         for node_id, labels_metrics in tolerances.items():
             for label, metrics in labels_metrics.items():
                 if len(metrics) > 1:
@@ -30,10 +31,9 @@ def calculate_tolerance(records: list) -> dict:
                     tolerance = stddev / mean
                 else:
                     tolerance = int(math.sqrt(metrics[0]))
-
+                    
                 tolerances[node_id][label] = tolerance
-
     except Exception as err:
-        logging.error(f"Tolerance calculation error : {err}")
+        logging.error(f"calculate_tolerance error : {err}")
 
     return tolerances
