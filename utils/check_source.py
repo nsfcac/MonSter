5a08ee9d5f6e856e07f2c547e02a38767efd7a9b
Cristiano Caon <cristiano.e.caon@gmail.com>
2022-08-03 20:35:14 -0500

    improved function

--- utils/check_source.py
+++ utils/check_source.py
@@ -1,4 +1,9 @@
 def check_source(source: str) -> str:
+    """Matches table name based on source.
+
+    :param str source: metric source.
+    :return str: table name.
+    """
     if source == "#Thermal.v1_4_0.Fan":
         table = "rpmreading"
     elif source == "#Thermal.v1_4_0.Temperature":
@@ -7,5 +12,7 @@ def check_source(source: str) -> str:
         table = "systempowerconsumption"
     elif source == "#Power.v1_3_0.Voltage":
         table = "voltagereading"
+    else:
+        table = ""
 
     return table
