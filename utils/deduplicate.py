78df5ff77a2ed36ece546f42cf8a21f2358f6bfb
Cristiano Caon <cristiano.e.caon@gmail.com>
2022-08-03 20:19:16 -0500

    improved function

--- utils/deduplicate.py
+++ utils/deduplicate.py
@@ -6,7 +6,11 @@ from utils.calculate_tolerance import calculate_tolerance
 
 
 def deduplicate(records: list) -> list:
+    """Deduplicates records list.
 
+    :param list records: original metrics.
+    :return list: deduplicated list from original metrics.
+    """
     previous_reading = {}
     deduplicated_records = []
 
@@ -24,13 +28,11 @@ def deduplicate(records: list) -> list:
                     nodeid = record[1]
                     label = record[3]
                     value = record[4]
-
                     if value is None or value < 0:
                         deduplicated_records.append(record)
                     else:
                         if nodeid not in previous_reading:
                             previous_reading[nodeid] = {}
-
                         if label not in previous_reading[nodeid]:
                             previous_reading[nodeid][label] = value
                             deduplicated_records.append(record)
@@ -41,11 +43,9 @@ def deduplicate(records: list) -> list:
                             if value < floor_tolerance or value > ceiling_tolerance:
                                 previous_reading[nodeid][label] = value
                                 deduplicated_records.append(record)
-
                 start_index = index
                 finish_time = curr_time.replace(microsecond=0, second=0, minute=0) + timedelta(hours=1)
-
     except Exception as err:
-        logging.error(f"Deduplication error : {err}")
-
+        logging.error(f"deduplicate error : {err}")
+        
     return deduplicated_records
