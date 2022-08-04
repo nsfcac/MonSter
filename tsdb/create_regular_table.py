ef7c6e6c56f657b310d0d5435cd592ca5ff86987
Cristiano Caon <cristiano.e.caon@gmail.com>
2022-08-03 20:28:04 -0500

    improved function

--- tsdb/create_regular_table.py
+++ tsdb/create_regular_table.py
@@ -3,13 +3,17 @@ import logging
 from utils.check_source import check_source
 
 
-def create_regular_table(conn: object, source: str) -> None:
-
+def create_regular_table(conn: object, source: str):
+    """Creates regular table based on source.
+    
+    :param conn object: connection object from psycopg2.
+    :param source str: metric source
+    """
     cursor = conn.cursor()
-
     try:
         table = check_source(source)
-
+        if not table:
+            return
         create_table_query = f"""CREATE TABLE IF NOT EXISTS {table} (
                 timestamp TIMESTAMPTZ NOT NULL,
                 nodeid INT4 NOT NULL,
@@ -17,12 +21,9 @@ def create_regular_table(conn: object, source: str) -> None:
                 fqdd TEXT,
                 value FLOAT4,
                 FOREIGN KEY (nodeid) REFERENCES public.nodes(nodeid));"""
-
         cursor.execute(create_table_query)
         conn.commit()
-
     except Exception as err:
-        logging.error(f"Create tables error : {err}")
-
+        logging.error(f"create_regular_table error : {err}")
     finally:
         cursor.close()
