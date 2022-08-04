56e914839e1e72ce61ec2fcbb66a6ef6c4d311bf
Cristiano Caon <cristiano.e.caon@gmail.com>
2022-08-03 20:26:43 -0500

    improved function

--- idrac_to_tsdb.py
+++ idrac_to_tsdb.py
@@ -12,10 +12,12 @@ from utils.check_config import check_config
 from utils.parse_config import parse_config
 
 logging.basicConfig(
-    level=logging.ERROR,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S %Z'
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S %Z"
 )
+
+logger = logging.getLogger("idrac_to_tsdb")
 
 sys.path.append(os.getcwd())
 
@@ -25,7 +27,7 @@ TSDB_CONFIG = dotenv_values(".env")
 
 CONNECTION_STRING = f"dbname={TSDB_CONFIG['DBNAME']} user={TSDB_CONFIG['USER']} password={TSDB_CONFIG['PASSWORD']} options='-c search_path=idrac8'"
 
-MEASUREMENTS = [
+SOURCES = [
     "#Thermal.v1_4_0.Fan",
     "#Thermal.v1_4_0.Temperature",
     "#Power.v1_4_0.PowerControl",
@@ -34,27 +36,30 @@ MEASUREMENTS = [
 
 
 def main():
+    """Fetches metrics from iDRAC8 components and stores them in TimescaleDB.
+    """
 
-    config_path = PATH + '/config.yml'
+    config_path = PATH + "/config.yml"
     config = parse_config(config_path)
 
     if not check_config(config):
         return
 
     conn = psycopg2.connect(CONNECTION_STRING)
-
     try:
-        idrac_config = config['idrac']
+        idrac_config = config["idrac"]
         idrac_datapoints = fetch_metrics(idrac_config)
-
-        for measurement in MEASUREMENTS:
-            create_regular_table(conn, measurement)
-            metrics = [metric for metric in idrac_datapoints if metric["source"] == measurement]
-            insert_metrics(conn, metrics, measurement)
-
+        logger.info("Fetched %s iDRAC8 datapoints", len(idrac_datapoints))
+        for source in SOURCES:
+            logger.info("Creating table for source %s if not exists", source)
+            create_regular_table(conn, source)
+            
+            metrics = [metric for metric in idrac_datapoints if metric["source"] == source]
+            
+            logger.info("Inserting %s metrics for %s source into table", len(metrics), source)
+            insert_metrics(conn, metrics, source)
     except Exception as err:
-        logging.error(f"main error : {err}")
-
+        logging.error(f"idrac_to_tsdb error : {err}")
     finally:
         conn.close()
 
