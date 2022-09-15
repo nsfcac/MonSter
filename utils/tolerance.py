2c1bb62817f51558268921dc6955906fe2168009
Cristiano Caon <cristiano.e.caon@gmail.com>
2022-09-15 17:35:33 -0500

    improved tolerance algorithm

--- utils/tolerance.py
+++ utils/tolerance.py
@@ -2,39 +2,67 @@ import logging
 import math
 import statistics
 
+from pandas import DataFrame
+from typing import Union
+
 logger = logging.getLogger("tolerance")
 
 
-def calculate_tolerance(records: list) -> dict:
+def calculate_tolerance(records: Union[list, DataFrame], formula: str = "cv") -> dict:
     """Calculates tolerances from given records list using Coefficient of Variation.
 
-    :param list records: records from table.
+    :param Union[list, DataFrame] records: records from table.
+    :param str formula: formula for tolerance calculation, defaults to "cv".
     :return dict: tolerances for each metric type.
     """
     tolerances = {}
 
     try:
-        for record in records:
-            node_id = record[1]
-            label = record[3]
-            value = record[4]
-            if node_id not in tolerances:
-                tolerances[node_id] = {}
-            if label not in tolerances[node_id]:
-                tolerances[node_id][label] = []
-            if value is not None and value > 0:
-                tolerances[node_id][label].append(value)
+        if isinstance(records, list):
+            for record in records:
+                node = record[1]
+                label = record[3]
+                value = record[4]
+                if node not in tolerances:
+                    tolerances[node] = {}
+                if label not in tolerances[node]:
+                    tolerances[node][label] = []
+                if value is not None and value > 0:
+                    tolerances[node][label].append(value)
+        else:
+            # Using Pandas DataFrame for bucketing
+            for record in records.itertuples():
+                node = record[2]
+                label = record[4]
+                value = record[5]
+                if node not in tolerances:
+                    tolerances[node] = {}
+                if label not in tolerances[node]:
+                    tolerances[node][label] = []
+                if value is not None and value > 0:
+                    tolerances[node][label].append(value)
                 
-        for node_id, labels_metrics in tolerances.items():
+        for node, labels_metrics in tolerances.items():
             for label, metrics in labels_metrics.items():
                 if len(metrics) > 1:
-                    stddev = statistics.stdev(metrics)
-                    mean = statistics.mean(metrics)
-                    tolerance = stddev / mean
-                else:
+                    if formula == "cv":
+                        # Coefficient of Variation formula
+                        stddev = statistics.stdev(metrics)
+                        mean = statistics.mean(metrics)
+                        tolerance = stddev / mean
+                    elif formula == "stddev":
+                        # Square root of standard deviation formula
+                        tolerance = int(math.sqrt(statistics.stdev(metrics)))
+                    elif formula == "mean":
+                        # Square root of mean formula
+                        tolerance = int(math.sqrt(statistics.mean(metrics)))
+                elif len(metrics) == 1:
                     tolerance = int(math.sqrt(metrics[0]))
+                else:
+                    tolerance = 0
                     
-                tolerances[node_id][label] = tolerance
+                tolerances[node][label] = tolerance
+                
     except Exception as err:
         logger.error("%s", err)
 
