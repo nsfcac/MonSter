ff6c28c52759083107372a7978059a0f80db0a48
artlands <lijie0416@gmail.com>
2024-04-29 11:41:20 -0500

    fix incorrect CORS config

--- mbuilder/mb_api.py
+++ mbuilder/mb_api.py
@@ -63,8 +63,8 @@ class Request(BaseModel):
 app = FastAPI()
 
 origins = [
-    "http://localhost:5000",
-    "http://127.0.0.1:5000"
+    "http://localhost:5500",
+    "http://127.0.0.1:5500"
 ]
 
 app.add_middleware(
